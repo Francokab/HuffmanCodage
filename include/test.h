9468ce2500b8016d0a6113c84abb20425cffdf4c
Ludovic <ludovicsenez861@gmail.com>
2023-10-18 13:14:31 +0200

    separate test and function files

---
+++ include/test.h
@@ -0,0 +1,13 @@
+#include <bitset>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+#include <string>
+
+#include "bitvector.h"
+#include "tree.h"
+
+void mainTest(string path);
+void compressbit_test();
