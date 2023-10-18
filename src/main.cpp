c6015963234a4423b6bb232f3f31072fdb4516f4
Francokab <francoiskabiry@yahoo.fr>
2023-10-18 13:37:58 +0200

    t

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,7 @@ using namespace std;
 
 int main() {
 
-<<<<<<< HEAD
   mainTest("text/text.txt");
-=======
-  BitVector bitData = read_file_bit("text/text.txt");
-
-  map<BitVector, int> dict_bit = count_frequency_bit(bitData, 8);
-
-  Btree tree = *dict_to_tree(dict_bit);
-
-  BitVector bitDataCompress = compress_text_static(bitData,tree);
-
-  ofstream flux("text/text_comp.txt");
-  flux << bit_to_string(bitDataCompress) << endl;
->>>>>>> cb802e6fe3195315ad2648067bc521740ad6f07a
 
   return 0;
 }
